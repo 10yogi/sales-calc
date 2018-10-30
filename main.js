afd5b7e2c1512c44d57cb50a1ebd3cedc6044bfc
Yogesh <kurmiyogi625@gmail.com>
2018-10-30 15:38:08 +0530

    modularize the main.js code

--- main.js
+++ main.js
@@ -1,55 +1,28 @@
-var exp = require('./sales_tax_exempted_items.js');
+var interface = require('./CreateInterface.js');
+var calc =require('./CalculateTax.js');
 
-const readline = require('readline');
-const fs1 = require('fs');
+var total_cost = 0.0,total_tax = 0.0;
 
-const rl = readline.createInterface({
-  input: fs1.createReadStream('sample.txt'),
-  crlfDelay: Infinity
-});
+interface.rl.on('line', (line) => {
+  line.trim();
+  var item = line.split(' ');
 
-var total_cost = 0.0;
-var total_tax = 0.0;
-const  basic_sales_tax_rate = 10;
-const  import_salse_tax_rate = 5;
+  var product = {
+    name:"",
+    price:parseFloat(item[item.length-1]),tax: 0.0,
+    price_with_tax: 0.0,
+    imported: false,exempted_from_tax:false,
+  };
+  
+  calc.calculate_tax(item,product);
 
+  total_tax  += product.tax;
+  total_cost += product.price_with_tax;
 
-rl.on('line', (line) => {
-	line.trim();
-
-	var item = line.split(' ');
-	var quantity = parseInt(item[0]);
-	var price = parseFloat(item[item.length-1]);
-
-	var is_imported  = false;
-	var is_exempted_from_salse_tax = false;
-
-	var product_name = "";
-	for(var i = 1;i<item.length-2;++i){
-		if(item[i]=='imported'){
-			is_imported = true;
-		}else{
-			if(exp.exempted_from_tax.has(item[i]))
-				is_exempted_from_salse_tax = true;
-			product_name+=" "+item[i];
-		}
-	}
-	var tax = 0;
-	var statement = quantity;
-	if(is_imported){
-		tax += price*import_salse_tax_rate/100;
-		statement+=" imported";
-	}
-	if(is_exempted_from_salse_tax==false){
-		tax+= price*basic_sales_tax_rate/100;
-	}
-	total_tax  += tax;
-	total_cost += price+tax;
-	statement+=product_name+": "+(price+tax).toFixed(2);
-	
-	console.log(statement);
+  console.log(item[0]+(product.imported?" imported":"")+" "+product.name+": "+(product.price_with_tax).toFixed(2));
 
 }).on('close',()=>{
-console.log("Salse Taxes: "+total_tax.toFixed(2));
-console.log("Total: "+total_cost.toFixed(2));
-process.exit(0);});
+    console.log("Salse Taxes: "+total_tax.toFixed(2));
+    console.log("Total: "+total_cost.toFixed(2));
+    process.exit(0);
+});
