2eea67784e335f732d6fd2ea7f2e148f6189717d
Yogesh <kurmiyogi625@gmail.com>
2018-10-30 05:39:08 +0530

    add sales_tax_exempted_items.js

---
+++ sales_tax_exempted_items.js
@@ -0,0 +1,15 @@
+
+exports.exempted_from_tax = new Set();
+
+exports.exempted_from_tax.add("book");
+exports.exempted_from_tax.add("books");
+exports.exempted_from_tax.add("novel");
+exports.exempted_from_tax.add("novels");
+exports.exempted_from_tax.add("food");
+exports.exempted_from_tax.add("chocolates");
+exports.exempted_from_tax.add("chocolate");
+exports.exempted_from_tax.add("pills");
+exports.exempted_from_tax.add("syringe");
+exports.exempted_from_tax.add("dressing");
+exports.exempted_from_tax.add("pill");
+
