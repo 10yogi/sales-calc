8fb6e5b6a141207bef5a08badcd8c90f80dea7fe
YOGESH KURMI <31936428+10yogi@users.noreply.github.com>
2018-10-31 13:10:26 +0530

    Update sales_tax_exempted_items.js

--- sales_tax_exempted_items.js
+++ sales_tax_exempted_items.js
@@ -1,8 +1,6 @@
 
 exports.basic_sales_tax_rate = 10,exports.import_salse_tax_rate = 5;
-
 exports.exempted_from_tax = new Set();
-
 exports.exempted_from_tax.add("book");
 exports.exempted_from_tax.add("books");
 exports.exempted_from_tax.add("novel");
